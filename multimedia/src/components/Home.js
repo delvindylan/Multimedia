5a6df482181b2df2b7bc17d646592b4d771357aa
Delvin <delvin@ngauv.ch>
2023-06-30 01:50:42 +0200

    navbar and image upload

---
+++ multimedia/src/components/Home.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Home () {
+
+
+    return (
+
+        <div>
+
+
+        <div className="homeTitle">
+
+            <h1>Delvin Photography</h1>
+
+        </div>
+
+
+        </div>
+
+    )
+}
---
+++ multimedia/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+
+export default function Navbar() {
+
+    
+
+    return(
+
+    <div>
+        <nav className="nav">
+            <Link to="/" className="site-title">Home</Link>
+            <ul>
+                <CustomLink to="/angebot">Angebot</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/kontakt">Kontakt</CustomLink>
+                <CustomLink to="/impressum">Impressum</CustomLink>
+            </ul>
+        </nav>
+    </div>
+
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+          <Link to={to} {...props}>
+            {children}
+          </Link>
+        </li>
+      )
+}
